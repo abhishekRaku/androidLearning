e0533b95e8512016abe76bcde4ecf8e5e0f0e5c4
abhishek.abhishek <abhishek.abhishek@rakuten.com>
2023-09-04 16:47:25 +0900

    saving instance for screen size

--- app/src/main/java/com/example/androidlearning/MainActivity.kt
+++ app/src/main/java/com/example/androidlearning/MainActivity.kt
@@ -1,25 +1,35 @@
 package com.example.androidlearning
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var goToBtn: Button
+    private lateinit var tvCount: TextView
+    private lateinit var btnCount: Button
+    private var count: Int = 0
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.i("Abhi","Activity1: onCreate")
 
         goToBtn = findViewById(R.id.goTobtn)
+        tvCount = findViewById(R.id.tvCount)
+        btnCount = findViewById(R.id.btnCount)
 
         goToBtn.setOnClickListener {
             val intent = Intent(this, MainActivity2::class.java)
             startActivity(intent)
         }
+
     }
 
     override fun onStart() {
@@ -30,6 +40,11 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         Log.i("Abhi","Activity1 : onResume")
+
+        btnCount.setOnClickListener {
+            count++
+            tvCount.setText(count.toString())
+        }
     }
 
     override fun onPause() {
@@ -51,4 +66,18 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
         Log.i("Abhi","Activity1 : onDestroy")
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("countInt",count)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val savedCount = savedInstanceState.getInt("countInt")
+        count = savedCount
+        tvCount.text = savedCount.toString()
+    }
+
+
 }
