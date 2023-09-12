716fc4fd73736b39fce307ec7574efc9d3dc841f
abhishek.abhishek <abhishek.abhishek@rakuten.com>
2023-09-12 15:28:43 +0900

    simple sharedPreference

--- app/src/main/java/com/example/androidlearning/MainActivity.kt
+++ app/src/main/java/com/example/androidlearning/MainActivity.kt
@@ -1,15 +1,39 @@
 package com.example.androidlearning
 
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.androidlearning.databinding.ActivityMainBinding
+import java.util.prefs.Preferences
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var sharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        sharedPreferences = getSharedPreferences("my_data", MODE_PRIVATE)
+
+        binding.btnSign.setOnClickListener {
+            if(binding.etvId.text.toString().isEmpty() || binding.etvPass.text.toString().isEmpty()){
+                Toast.makeText(this,"Field cannot be empty",Toast.LENGTH_SHORT).show()
+            }else{
+                saveUserInstance(binding.etvId.text.toString())
+                val intent = Intent(this,HomePage::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+
+    private fun saveUserInstance(value: String) {
+        sharedPreferences.edit().apply {
+            putBoolean(UserSettings.LOGGED_IN.value,true)
+            putString(UserSettings.USER_ID.value,value)
+            commit()
+        }
     }
 
 }
